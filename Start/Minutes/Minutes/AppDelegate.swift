bb2e399afb54c94b1436c3c805cd100e9d042ffc
Mehedi Hasan <m.mehedi_hasan@hotmail.com>
2020-04-07 03:34:21 +0600

    commit 3

--- Start/Minutes/Minutes/AppDelegate.swift
+++ Start/Minutes/Minutes/AppDelegate.swift
@@ -1,5 +1,10 @@
 import UIKit
-
+import AppCenter
+import AppCenterDistribute
+import AppCenterCrashes
+import AppCenterAnalytics
+import AppCenterPush
+//e2c9c369-e455-4867-b77c-f63bb3b066f3
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate
 {
@@ -8,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-
+        MSAppCenter.start("your-app-secret", withServices: [MSDistribute.self, MSCrashes.self, MSAnalytics.self, MSPush.self])
         return true
     }
 
